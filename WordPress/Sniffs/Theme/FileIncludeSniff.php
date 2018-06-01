400ae13c234f5def17a371e3a0da087082390902
jrfnl <jrfnl@users.noreply.github.com>
2018-06-01 03:19:39 +0200

    FileInclude: add "file(s) allowed to be included/required" feature
    This PR was inspired by PR 104.
    
    The `FileInclude` sniff already contained a whitelist for files from which `include`/`require` calls are expected and allowed.
    
    However, files are often enough included/required from arbitrarily named files in a theme and there are certain typical library files, such as the TGMPA file, which are always fine to be included.
    
    This PR adds the functionality needed to also have a whitelist of files which are allowed to be included from any file.
    
    The sniff will only test the *last* text string in an include/require statement and will only accept a file as "allowed to be included" if the file name is either the complete string or at the end of the text string preceded by a `/`.
    
    Includes unit test.
    
    Other notes:
    * Minor efficiency fix. Once it has been established that a file is on the "whitelist for files from which `include`/`require` calls are expected", this sniff will not be called again for that file.
    * The "whitelist for files from which `include`/`require` calls are expected" functionality was not yet unit tested. A test has been added for this.
    * Left the TGMPA file in the "whitelist for files from which `include`/`require` calls are expected" as well as it includes WP core files if/when needed.
    * Changed the errorcode from `FileIncludeFound` to `Found` as the name of the sniff is always included in the errorcode anyway, so this was superfluous.
    * As the name of the file being included/required will now be checked, added a safeguard against throwing the error too early during live coding + added a unit test for this.
    
    Closes 104

--- WordPress/Sniffs/Theme/FileIncludeSniff.php
+++ WordPress/Sniffs/Theme/FileIncludeSniff.php
@@ -24,13 +24,40 @@ use PHP_CodeSniffer_Tokens as Tokens;
 class FileIncludeSniff extends Sniff {
 
 	/**
-	 * A list of files to skip.
+	 * Regex template for the "files allowed for inclusion" check.
+	 *
+	 * This regex makes sure that the filename is either the complete string
+	 * OR at the very end of the string, but preceded by a `/`.
+	 *
+	 * @var string
+	 */
+	const ALLOWED_REGEX_TEMPLATE = '`(?:^|/)(?:%s)$`';
+
+	/**
+	 * A list of file from which include/require calls are allowed/expected.
 	 *
 	 * @var array
 	 */
 	protected $file_whitelist = array(
-		'functions.php' => true,
+		'functions.php'                   => true,
+		'class-tgm-plugin-activation.php' => true,
 	);
+
+	/**
+	 * A list of non-template files which are expected to be `include`d/`require`d.
+	 *
+	 * @var array
+	 */
+	protected $allowed_for_inclusion = array(
+		'class-tgm-plugin-activation.php',
+	);
+
+	/**
+	 * Internal storage for the "files allowed for inclusion" regex once parsed.
+	 *
+	 * @var string
+	 */
+	private $allowed_regex = '';
 
 	/**
 	 * Returns an array of tokens this test wants to listen for.
@@ -38,6 +65,17 @@ class FileIncludeSniff extends Sniff {
 	 * @return array
 	 */
 	public function register() {
+		// Prepare the "files allowed for inclusion" regex only once.
+		$regex_parts = array();
+		foreach ( $this->allowed_for_inclusion as $file_name ) {
+			$regex_parts[] = preg_quote( $file_name, '`' );
+		}
+		$regex_parts = array_filter( $regex_parts );
+
+		if ( ! empty( $regex_parts ) ) {
+			$this->allowed_regex = sprintf( self::ALLOWED_REGEX_TEMPLATE, implode( '|', $regex_parts ) );
+		}
+
 		return Tokens::$includeTokens;
 	}
 
@@ -47,17 +85,45 @@ class FileIncludeSniff extends Sniff {
 	 * @param int $stackPtr The position of the current token in the stack.
 	 */
 	public function process_token( $stackPtr ) {
-		$token     = $this->tokens[ $stackPtr ];
-		$file_name = basename( $this->phpcsFile->getFileName() );
 
-		if ( ! isset( $this->file_whitelist[ $file_name ] ) ) {
-			$this->phpcsFile->addWarning(
-				'Check that %s is not being used to load template files. "get_template_part()" should be used to load template files.',
-				$stackPtr,
-				'FileIncludeFound',
-				array( $token['content'] )
-			);
+		$file_name = basename( $this->phpcsFile->getFileName() );
+		if ( defined( 'PHP_CODESNIFFER_IN_TESTS' ) ) {
+			// Allow for the whitelisted file functionality to be unit tested.
+			$file_name = str_replace( '.inc', '.php', $file_name );
 		}
+
+		if ( isset( $this->file_whitelist[ $file_name ] ) ) {
+			// Whitelisted file. We can ignore any other include/require statements found in this file.
+			return $this->phpcsFile->numTokens;
+		}
+
+		$end = $this->phpcsFile->findNext( array( T_SEMICOLON, T_CLOSE_TAG ), ( $stackPtr + 1 ) );
+		if ( false === $end ) {
+			// Live coding/parse error.
+			return;
+		}
+
+		if ( '' !== $this->allowed_regex ) {
+			for ( $i = $end; $i > $stackPtr; $i-- ) {
+				if ( isset( Tokens::$stringTokens[ $this->tokens[ $i ]['code'] ] ) === false ) {
+					continue;
+				}
+
+				if ( preg_match( $this->allowed_regex, $this->strip_quotes( $this->tokens[ $i ]['content'] ) ) > 0 ) {
+					// File which is allowed to be included/required.
+					return;
+				}
+
+				break;
+			}
+		}
+
+		$this->phpcsFile->addWarning(
+			'Check that %s is not being used to load template files. "get_template_part()" should be used to load template files.',
+			$stackPtr,
+			'Found',
+			array( $this->tokens[ $stackPtr ]['content'] )
+		);
 	}
 
 }
--- WordPress/Tests/Theme/FileIncludeUnitTest.php
+++ WordPress/Tests/Theme/FileIncludeUnitTest.php
@@ -20,6 +20,22 @@ use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 class FileIncludeUnitTest extends AbstractSniffUnitTest {
 
 	/**
+	 * Get a list of all test files to check.
+	 *
+	 * @param string $testFileBase The base path that the unit tests files will have.
+	 *
+	 * @return string[]
+	 */
+	protected function getTestFiles( $testFileBase ) {
+		$extra_files_path = dirname( $testFileBase ) . DIRECTORY_SEPARATOR . 'FileInclude' . DIRECTORY_SEPARATOR;
+
+		return array(
+			$testFileBase . 'inc',
+			$extra_files_path . 'functions.inc',
+		);
+	}
+
+	/**
 	 * Returns the lines where errors should occur.
 	 *
 	 * @return array <int line number> => <int number of errors>
@@ -31,17 +47,30 @@ class FileIncludeUnitTest extends AbstractSniffUnitTest {
 	/**
 	 * Returns the lines where warnings should occur.
 	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
 	 * @return array <int line number> => <int number of warnings>
 	 */
-	public function getWarningList() {
-		return array(
-			3 => 1,
-			4 => 1,
-			5 => 1,
-			6 => 1,
-			7 => 1,
-			8 => 1,
-		);
+	public function getWarningList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'FileIncludeUnitTest.inc':
+				return array(
+					3  => 1,
+					4  => 1,
+					5  => 1,
+					6  => 1,
+					7  => 1,
+					8  => 1,
+					11 => 1,
+					12 => 1,
+					13 => 1,
+					14 => 1,
+				);
+
+			case 'functions.inc':
+			default:
+				return array();
+		}
 	}
 
 }
